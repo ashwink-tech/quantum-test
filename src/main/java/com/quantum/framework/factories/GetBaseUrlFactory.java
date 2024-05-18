d1bd648778869772efd51d8594c78305609bf513
Ashwin Karangutkar <ashwinkarangutkar@192.168.1.3>
2024-05-18 23:15:17 +0530

    Added Url Factory

---
+++ src/main/java/com/quantum/framework/factories/GetBaseUrlFactory.java
@@ -0,0 +1,12 @@
+package com.quantum.framework.factories;
+
+import static java.lang.System.getProperty;
+
+public class GetBaseUrlFactory {
+
+  private GetBaseUrlFactory() {}
+
+  public static String getBaseUrl() {
+    return getProperty("baseurl");
+  }
+}
