42a6cb207c4dbd1d635f63fbd7837fed2ea9004f
Ashwin Karangutkar <ashwinkarangutkar@192.168.1.3>
2024-05-18 23:16:14 +0530

    Added Cucumber Helper

---
+++ src/main/java/com/quantum/framework/helper/CucumberHelper.java
@@ -0,0 +1,35 @@
+package com.quantum.framework.helper;
+
+import com.quantum.framework.utilities.LogUtil;
+import io.cucumber.datatable.DataTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.quantum.framework.utilities.LogUtil.*;
+import static java.lang.Thread.currentThread;
+
+public class CucumberHelper {
+
+  private CucumberHelper() {
+    throw new IllegalStateException("CucumberHelper class");
+  }
+
+  private static ConcurrentHashMap<Integer, String> scenarioMap = new ConcurrentHashMap<>();
+
+  public static List<Map<String,String>> getDataTable(DataTable table) {
+    info("Returning the value from Cucumber Table");
+    return table.asMaps(String.class, String.class);
+  }
+
+  // Add the scenario in scenario map
+  public static void addScenario(String scenario) {
+    scenarioMap.put(currentThread().hashCode(), scenario);
+  }
+
+  // Get the scenario from scenario map to name the tests in Aerokube
+  public static String getScenario() {
+    return scenarioMap.get(currentThread().hashCode());
+  }
+}
