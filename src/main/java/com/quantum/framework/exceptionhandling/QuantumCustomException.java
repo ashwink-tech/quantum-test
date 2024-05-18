f98f7ecc517d003ffed47074d4891c2dd525b8f7
Ashwin Karangutkar <ashwinkarangutkar@192.168.1.3>
2024-05-18 23:14:48 +0530

    Added Custom Exceptions

---
+++ src/main/java/com/quantum/framework/exceptionhandling/QuantumCustomException.java
@@ -0,0 +1,7 @@
+package com.quantum.framework.exceptionhandling;
+
+public class QuantumCustomException extends RuntimeException {
+    public QuantumCustomException(String message) {
+        super(message);
+    }
+}
